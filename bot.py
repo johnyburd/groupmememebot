39c7c54fa744b6614ecda20d834e8c6c8eb7e87b
Jonathan Buchanan <johnyburd8@gmail.com>
2016-06-14 17:27:59 -0500

    updates and improvements

--- bot.py
+++ bot.py
@@ -4,8 +4,9 @@ import re
 import urllib.request as urllib2
 import os
 import re
-import datetime
+#import datetime
 import random
+import time
 
 import groupy
 from groupy import Bot
@@ -47,50 +48,56 @@ members = groupy.Member.list()
 bots = groupy.Bot.list()
 group = groups.first
 bot = bots.first
+#Bot.create("twinkle_toes97", group, "https://i.groupme.com/1125x1500.jpeg.7e3ad500b839434c9eb42494790a9f10")
 #print("joined " + group)
 
-lastmsg = ""
-lasttime = datetime.datetime.now()
+lastmsg = group.messages().newest
+lasttime = time.time()
 
 while True:
     messages = group.messages()
     message = messages.newest
     del messages[15:]
     for m in messages:
-        if (m.name != User.get().name and len(m.likes()) > 2):
+        if (str(m.name) != str(User.get().name) and len(m.likes()) > 2 and str(m.name) != "twinkle_toes97"):
             like = True
             for l in m.likes():
                 if (str(l) == str(User.get().name)):
-                    print(l)
                     like = False
             if (like):
                 m.like()
 
-    hours = datetime.datetime.now().time().hour
-    if (message != lastmsg):
-        lasttime = datetime.datetime.now()
+    if (str(message) != str(lastmsg)):
+        #lasttime = datetime.datetime.now()
+        lasttime = int(time.time())
         print(message)
         lastmsg = message
-    
+    now = int(time.time())
+    d = divmod(now - lasttime,86400)  # daystt
+    h = divmod(d[1],3600)  # hours
+    m = divmod(h[1],60)  # minutes
+    s = m[1]  # seconds
+    print ('%d days, %d hours, %d minutes, %d seconds' % (d[0],h[0],m[0],s))
+
+    if (d[0] < 1 and h[0] < 1 and m[0] > 2 and m[0] < 10):
+        if (random.randint(0,80000) == 0):
+            pull_images("dank meme")
+
+
+
+
+
     if (message.name != User.get().name):
-        m = re.match(r"(.*)?give me a meme about (.*)", str(message), flags=re.I)
+        m = re.match(r"(.*)meme(s)? .*about (.*)", str(message), flags=re.I)
         if m:
-            pull_images(m.group(2) + " meme")
+            pull_images(m.group(3) + " meme")
+            print("found: " + m.group(3))
 
-        filenames = next(os.walk(PATH))[2]
-        for f in filenames:
-            image_attachment = attachments.Image.file(open(PATH + f, 'rb'))
-            bot.post("",image_attachment.url)
-            os.remove(PATH + f)
-
-
- 
-        
+    filenames = next(os.walk(PATH))[2]
+    for f in filenames:
+        image_attachment = attachments.Image.file(open(PATH + f, 'rb'))
+        bot.post("",image_attachment.url)
+        os.remove(PATH + f)
 
 
-   
-
-   
-
-    
 
